ab6bf7e33c5f63e664021f83a6921e0e4980925b
arisetton <ajsetton@gmail.com>
2016-12-27 11:03:31 -0300

    Update calc.js

--- js/calc.js
+++ js/calc.js
@@ -5,13 +5,15 @@ function calcular() {
         isPatagonico = $("input[name='patagonico']:checked").val(),
         valorAlquiler = $("#alquiler").val(),
         deduccionAlquiler = 12 * valorAlquiler * .4 > TOPE_ALQUILER ? TOPE_ALQUILER : 12 * valorAlquiler * .4,
-        hijosElement = document.getElementById("hijos"),
+        valorHipotecario = $("#hipotecario").val(),
+        deduccionHipotecario = 12 * valorHipotecario > TOPE_HIPOTECARIO ? TOPE_HIPOTECARIO : 12 * valorHipotecario,
+		hijosElement = document.getElementById("hijos"),
         cantHijos = hijosElement.options[hijosElement.selectedIndex].value,
         sueldoNeto = 0 == isJubilado ? .17 * sueldoBruto > TOPE_APORTES ? sueldoBruto - TOPE_APORTES : .83 * sueldoBruto : .06 * sueldoBruto > TOPE_APORTES ? sueldoBruto - TOPE_APORTES : .94 * sueldoBruto;
 
     var sueldoNetoAnual = 13 * sueldoNeto,
         mniConDeduccionEspecial = (MINIMO_NO_IMPONIBLE + ADICIONAL_4TA_CATEGORIA) * (1.22 * isPatagonico + (1 - isPatagonico));
-    mniTotal = (mniConDeduccionEspecial + CONYUGE * isConyuge + HIJO * cantHijos + deduccionAlquiler) * (1 - isJubilado) + isJubilado * (TOPE_JUBILADO + deduccionAlquiler),
+    mniTotal = (mniConDeduccionEspecial + CONYUGE * isConyuge + HIJO * cantHijos + deduccionAlquiler + deduccionHipotecario) * (1 - isJubilado) + isJubilado * (TOPE_JUBILADO + deduccionAlquiler + deduccionHipotecario),
         montoImponibleAplicable = 0,
         mniTotal < sueldoNetoAnual && (montoImponibleAplicable = sueldoNetoAnual - mniTotal);
     for (var n = [0, 0, 0, 0, 0, 0, 0, 0, 0], o = 0; o < n.length && (n[o] = calcularValorEscala(o, montoImponibleAplicable), n[o] == fijosEscalas[o]); o++);
@@ -54,4 +56,5 @@ var topesEscalas = [2e4, 4e4, 6e4, 8e4, 12e4, 16e4, 24e4, 32e4, 99999999],
     HIJO = 24432,
     TOPE_APORTES = 10896.32,
     TOPE_JUBILADO = 407592,
-    TOPE_ALQUILER = 51967;
+    TOPE_ALQUILER = 51967,
+	TOPE_HIPOTECARIO = 20000;
